886c87bb4a8d8d055ed910b7c0dffce4970fc2fc
Yair Elitzur <110463095+yairelit@users.noreply.github.com>
2026-01-31 22:20:39 +0200

    Change data loading from Excel to CSV format

--- source/io.py
+++ source/io.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 def load_and_clean_data(filepath):
-    df = pd.read_excel(filepath)
+    df = pd.read_csv(filepath)
     df = df[(df["start year"] != 0) & (df["start month"].between(1, 12))]
     drop_cols = ["Lamas num","case num","road num","semel yishuv","shem yishuv","shem medaveach","road name"]
     return df.drop(columns=drop_cols)
